9a62623ebb6f519dda362aef4273fae4faadf7f3
abdulmajeed229 <johnharris5934@gmail.com>
2024-03-08 20:27:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+var password1 = prompt("Enter First Password");
+
+var password2 = prompt("Enter Last Pssword")
+
+var pass = 123456;
+
+
+if (password1 == pass) {
+
+    if (password2 == pass)
+
+        document.write("Correct")
+
+} else {
+    document.write("Wrong");
+}
+
+
